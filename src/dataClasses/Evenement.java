8ebadf5f4b5d691d21bd536d830c982d22f170d0
Ludo <Ludo@PC-de-ludo>
2018-02-13 13:27:41 +0100

    Hello world test commit

---
+++ src/dataClasses/Evenement.java
@@ -0,0 +1,20 @@
+package dataClasses;
+
+public class Evenement {
+	
+	private int niveaux;
+	private String type;
+	
+	public int getNiveaux() {
+		return niveaux;
+	}
+	public void setNiveaux(int niveaux) {
+		this.niveaux = niveaux;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+}
---
+++ src/dataClasses/Portefeuille.java
@@ -0,0 +1,5 @@
+package dataClasses;
+
+public class Portefeuille {
+
+}
